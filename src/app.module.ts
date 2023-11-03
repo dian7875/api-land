586c4754efb893dd73179cfc3554d80a7dbb335c
Dian7875 <dian7875.a@gmail.com>
2023-11-03 11:58:30 -0600

    Fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         port: 3306,
         username: 'root',
         password:'root',
-        database:'CompanyRYC',
+        database:'companyryc',
         autoLoadEntities: true,
         synchronize: true
       }),
