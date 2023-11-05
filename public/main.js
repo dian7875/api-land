ae62c79fe77dbe649f2b88708244d4fa7b77d17c
Dian7875 <dian7875.a@gmail.com>
2023-11-04 22:06:03 -0600

    Parte 1 Pag Admin

--- public/main.js
+++ public/main.js
@@ -2,3 +2,32 @@ window.addEventListener("load", function (event) {
     console.log("'Todos los recursos terminaron de cargar!");
   });
   
+  let edit = this.document.getElementById("AgregarServ");
+  
+ edit.addEventListener("click",function(){
+  document.getElementById("Servicios-Create").style.visibility="visible";
+  
+ })
+  
+    
+  
+
+    /*const cardText = document.getElementById("input");
+    let createCard = document.getElementById("BTN")
+
+    createCard.addEventListener("click", function() {
+        const text = cardText.value;
+        if (text.trim() !== "") {
+            // Crear una nueva tarjeta
+            const my_card = document.createElement("my_cards");
+            my_card.className = "my_card";
+            my_card.textContent = text;
+            
+            // Agregar la tarjeta al contenedor
+            my_cards.appendChild(my_card);
+            
+            // Limpiar el campo de entrada de texto
+            cardText.value = "";
+        }
+    });
+*/ 
